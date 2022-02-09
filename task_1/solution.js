05ecc91297f0584976fc42120af44cc99f8ad063
Mariy Kromina <avmbio@yandex.ru>
2022-02-09 21:05:09 +0300

    add ! теперь в корзине и пробелы Р

--- task_1/solution.js
+++ task_1/solution.js
@@ -11,7 +11,7 @@ function addInCartMessage(title, price) {
     message = message + " ";// добавьте в конец переменной message пробел
     message = message + productPrice;// добавьте в конец переменной message значение переменной productPrice
     message = message + " ";// добавьте в конец переменной message пробел
-    message = message + "теперь в корзине";// добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + "теперь в корзине!";// добавьте в конец переменной message текст 'теперь в корзине!'
     console.log(message) ;// выведите переменную message в консоль
 
     // Конец решения задания №1.1.
@@ -40,9 +40,9 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     let newSum
-    newSum = oldSum+difference;// создайте переменную newSum и присвойте ей сумму oldSum и difference
-    newSumText = newSum +'₽';// создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    newSumText = `${newSum}₽`;// замените склеивание, используемое в newSum Text на шаблонную строку
+    newSum = oldSum + difference;// создайте переменную newSum и присвойте ей сумму oldSum и difference
+    newSumText = newSum + '₽';// создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    newSumText = `${newSum}  ₽`;// замените склеивание, используемое в newSum Text на шаблонную строку
 
     // Конец решения задания №1.3.
 
