f3742ed3934a843100756ca780eb0a0c6926876d
Mariy Kromina <avmbio@yandex.ru>
2022-02-14 15:04:25 +0300

    delet 68

--- task_2/solution.js
+++ task_2/solution.js
@@ -65,8 +65,8 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let freeShipping; // создайте переменную freeShipping
                                        // запишите без использования if или любых других условий:
     
-    Boolean (shippingSum = ! 0) ;// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-    freeShipping = !Boolean(shippingSum);
+                                     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    freeShipping = !Boolean(shippingSum = 0);
 
     // Конец решения задачи №2.3.
 
