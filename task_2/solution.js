69ff36ce2b83c8ca2177b34ce1da78a49ae10db8
Mariy Kromina <avmbio@yandex.ru>
2022-02-14 15:00:47 +0300

    Boolean (shippingSum = ! 0)

--- task_2/solution.js
+++ task_2/solution.js
@@ -65,7 +65,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let freeShipping; // создайте переменную freeShipping
                                        // запишите без использования if или любых других условий:
     
-    Boolean (shippingSum = ! 0 , freeShipping) ;// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    Boolean (shippingSum = ! 0) ;// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
     freeShipping = !Boolean(shippingSum);
 
     // Конец решения задачи №2.3.
