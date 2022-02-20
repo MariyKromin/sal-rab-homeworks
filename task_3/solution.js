6c5a7b550fd7544b359986c6867d819973dbac78
Mariy Kromina <avmbio@yandex.ru>
2022-02-20 16:49:49 +0300

    17

--- task_3/solution.js
+++ task_3/solution.js
@@ -14,7 +14,7 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-   itemCountText = " " ; // 1. Создайте переменную itemCountText, равную пустой строке
+   itemCountText = "  " ; // 1. Создайте переменную itemCountText, равную пустой строке
    itemCountText = itemCountText + item.count ; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
    itemCountText = itemCountText + ` x ` ; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
    itemCountText = itemCountText + item.price ;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
