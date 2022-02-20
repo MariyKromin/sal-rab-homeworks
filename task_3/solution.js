5b04722216275de5ba30a20bfb0bf22833eb27d2
Mariy Kromina <avmbio@yandex.ru>
2022-02-20 16:51:02 +0300

    17

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,7 +3,7 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    item = {id: 1, title : `Пицца`, count: 5, price: `500.00`} ;// product - объект вида {id: id, title: title, count: count, price: price}
+    item = {id: 1, title : `Пицца`, count:5, price: `500.00`} ;// product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
@@ -14,7 +14,7 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-   itemCountText = "  " ; // 1. Создайте переменную itemCountText, равную пустой строке
+   itemCountText = "" ; // 1. Создайте переменную itemCountText, равную пустой строке
    itemCountText = itemCountText + item.count ; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
    itemCountText = itemCountText + ` x ` ; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
    itemCountText = itemCountText + item.price ;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
