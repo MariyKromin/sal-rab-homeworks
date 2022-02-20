c2212960c80ce219792a81e38a196f65f598c314
Mariy Kromina <avmbio@yandex.ru>
2022-02-20 21:23:12 +0300

    23 and 24

--- task_3/solution.js
+++ task_3/solution.js
@@ -20,8 +20,8 @@ function renderCartItem(item) {
    itemCountText = itemCountText + ` × ` ; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
    itemCountText = itemCountText + product.price ;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
    itemCountText = itemCountText + ` ₽ = ` ;// 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-   let sum ;
-   sum = product.count * product.price ;// 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+   
+   let sum = product.count * product.price ;// 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
    itemCountText = itemCountText + sum  ;// 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
    itemCountText = itemCountText + ` ₽`; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
