2821223ef41d4b2b5a70f1f93350051e41f285a2
Mariy Kromina <avmbio@yandex.ru>
2022-02-25 20:02:37 +0300

    49

--- task_final/solution.js
+++ task_final/solution.js
@@ -46,7 +46,7 @@ function sendRequest(name, phone, address, goods, sum) {
     data.order.sum = sum;
 
     data.client = name + " " + phone;
-    data.goods = data[goods.title, goods.count];
+    data.goods = [{title, count}];
     
 
     let jsonData = JSON.stringify({data: data}); 
