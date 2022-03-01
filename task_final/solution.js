10c82714ffdaa15b5633c507e535f5d76bea6e40
Mariy Kromina <avmbio@yandex.ru>
2022-03-01 15:28:46 +0300

    44

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,24 @@
 //    }ntr
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone},order: {address, sum}, goods:[title, count]};
-     
+//function sendRequest(name, phone, address, goods, sum) {
+  //  let data = {client: {name, phone},order: {address, sum}, goods:[title, count]};
+  function sendRequest(name, phone, address, goods, sum) {
+    let data = {goods: [], order: {}};
+ 
     let countOfGoods = goods.length;
      
-    for (let i = 0; i <= countOfGoods; i++) {
-        data.goods.push(goods[i].title);
-        data.goods.push(goods[i].count);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.title=(goods[i].title);
+        data.goods.count=(goods[i].count);
+        data.data.push (data.goods)
     }
 
     data.order.address = "ул."+ " " + address.street + "," + " " +  "дом" + " " + address.house + "," + " " + address.entrance + " " + "подъезд" + "," + " " + address.floor + " " + "этаж"  + "," + " " + "кв" + " " + address.flat;
     data.order.sum = sum;
 
     data.client = name + " " + phone;
-    data.goods = [title, count];
+    
     
 
     let jsonData = JSON.stringify({data: data}); 
